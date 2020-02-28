ffa6376dc71348ff1566111e885a82ec4341c584
BenHall-7 <benjaminjahall@gmail.com>
2020-02-27 22:00:36 -0500

    Add axiosWithAuth and PrivateRoute; start app routes

--- dev-desk/src/App.js
+++ dev-desk/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        {/* Set the /register path to display the registration component */}
+        <Route exact path="/register" component={() => "Registration here"}/>
+        {/* Set the private route to handle the main application*/}
+        <PrivateRoute exact path="/" component={() => "PrivateRoute here"}/>
+        {/* Add more as needed */}
+      </Switch>
     </div>
   );
 }
