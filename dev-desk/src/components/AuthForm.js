9e4db275fe903f9436fc9bf032a61a9baffa3610
unknown <wadecoplen@gmail.com>
2020-02-27 20:14:35 -0700

    Commit 3: Combine Register and Login into one form

--- dev-desk/src/components/AuthForm.js
+++ dev-desk/src/components/AuthForm.js
@@ -1,37 +1,39 @@
 import React from 'react'; 
 import axios from 'axios';
 
-export default function Register() {    
-    const [registrationInformation, setRegistrationInformation]= ({
-        firstName: '',
-        lastName: '',
+export default function AuthForm() {    
+    const [authInfo, setAuthInfo]= ({
+        fullName: '',
         email: '',
         password: ''
     })
-    axios
-    .post(url.id, registrationInformation)
-    .then(response => {
-        console.log('post response', response)
-        localStorage.setItem('token', response.data.token)
-    })
-    .catch(error => (error));
+    const handleChange = e => {
+        e.preventDefault();
+        setAuthInfo({...authInfo, [e.target.name]: e.target.value})
+    }
+    const handleSubmit = e => {
+        e.preventDefault();  
+          console.log('something');
+        axios
+        .post(`url/${role}`, authInfo)
+        .then(response => {
+            console.log('post response', response)
+            localStorage.setItem('token', response.data.token)
+        })
+        .catch(error => (error));
+        }
     
 
     return(
         <div>
             <form onSubmit= {handleSubmit}>
                 <input
-                    name= 'firstName'
+                    name= 'fullName'
                     type= 'text'
                     value= {handleChange}
                     placeholder= 'First Name'
                 />
-                <input
-                    name= 'lastName'
-                    type= 'text'
-                    value= {handleChange}
-                    placeholder= 'Last Name'
-                />
+               
                 <input
                     name= 'email'
                     type= 'text'
