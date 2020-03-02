eea3b85a3f08b53fdc534a20b3378d29e8e1795e
BenHall-7 <benjaminjahall@gmail.com>
2020-03-01 23:41:53 -0500

    Cleanup AuthForm structure and move to logged-in state

--- dev-desk/src/components/AuthForm.js
+++ dev-desk/src/components/AuthForm.js
@@ -6,30 +6,50 @@ export default function AuthForm({role, history}) {
     const [authInfo, setAuthInfo]= useState({
         username: '',
         password: '',
-        type: ''
+        type: 'user'
     })
     const handleChange = e => {
-        e.preventDefault();
-        setAuthInfo({...authInfo, [e.target.name]: e.target.value})
+        setAuthInfo({...authInfo, [e.target.name]: e.target.value});
     }
     const handleSubmit = e => {
         e.preventDefault();  
-          console.log('something');
-        axios
-        .post(`https://daniels-dev-desk-backend.herokuapp.com/api/auth/${role}`, authInfo)
-        .then(response => {
-            console.log('post response', response)
-            localStorage.setItem('token', response.data.token)
-        })
-        .catch(error => (error));
+        axios.post(`https://daniels-dev-desk-backend.herokuapp.com/api/auth/${role}`, authInfo)
+            .then(response => {
+                localStorage.setItem('token', response.data.token);
+                history.push("/");
+            })
+            .catch(console.log);
     }
         
         
     return(
-        
-        
         <div className= 'AuthForm'>
+            {role === 'login' && <Link to= '/register'>Registration Page</Link>}
+            {role === 'register' && <Link to='/login'>Login Page</Link>}
             <form onSubmit= {handleSubmit}>
+                {/* This div specifically for the radio buttons */}
+                {role === 'register' && <div className="user-type-radio">
+                    <input
+                        type='radio'
+                        name='type'
+                        id='user'
+                        value='user'
+                        checked={authInfo.type === 'user'}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor='user'>User</label>
+
+                    <input
+                        type='radio'
+                        name='type'
+                        id='admin'
+                        value='admin'
+                        checked={authInfo.type === 'admin'}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor='admin'>Helper</label>
+                </div>}
+
                 <input
                     name= 'username'
                     type= 'text'
@@ -45,27 +65,8 @@ export default function AuthForm({role, history}) {
                     onChange= {handleChange}
                     placeholder= 'password'
                 />
-                
-                
-                {role === 'register' ? 
-                <>
-                <input
-                    name= 'type'
-                    type= 'text'
-                    onChange= {handleChange}
-                    value= {authInfo.type}
-                    placeholder= 'type'
-                    
-                />  
-                <Link to= '/login'>Login</Link> 
-                </>
-                : null}
-                {role === 'login' ?
-                <Link to= '/register'>Click here to Register</Link>       
-                : null}
                 <button type= 'submit'>Submit</button>   
             </form>
-
         </div>
     )
 }
