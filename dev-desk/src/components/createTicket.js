6b4e150ac6156cefab7bcba161b1baa99a2ba202
BenHall-7 <benjaminjahall@gmail.com>
2020-03-05 22:52:12 -0500

    Fix some bracket/formatting issues

--- dev-desk/src/components/createTicket.js
+++ dev-desk/src/components/createTicket.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import axiosWithAuth from 'axios';
 
 
- const CreateTicket = () => {
+const CreateTicket = () => {
     const [creatingTicket, setCreatingTicket] = useState({
         user_id: '',
         description: "",
@@ -13,72 +13,69 @@ import axiosWithAuth from 'axios';
         solved_by: "",
         username: ""
     })
+
     const handleSubmit = e => {
     axiosWithAuth()
     .post('https://daniels-dev-desk-backend.herokuapp.com/api/tickets', creatingTicket)
     .then(response => {
-        console.log('response', response);
-        history.push('/ResolveTicket')
-      
-      
-})
+        console.log('response', response);        
+    })};
     
-    
-return(
-    <div>
-        <form onSubmit = {handleSubmit}>
-            <input
-                name= 'user_id'
-                type= 'text'
-                value= ''
-                onChange= ''
-            />
-            <input
-                name= 'description'
-                type= 'text'
-                value= ''
-                onChange= ''
-            />
-            <input
-                name= 'category'
-                type= 'text'
-                value= ''
-                onChange= ''
-            />
-            <input
-                name= 'reply'
-                type= ''
-                value= ''
-                onChange= ''
-            />
-            <input
-                name= 'solved'
-                type= ''
-                value= ''
-                onChange= ''
-            />  
-            <input
-                name= 'category'
-                type= ''
-                value= ''
-                onChange= ''
-            />
-            <input
-                name= 'solved_by'
-                type= ''
-                value= ''
-                onChange= ''
-            />
-            <input
-                name= 'username'
-                type= ''
-                value= ''
-                onChange= ''
-            />
-            <button type= 'submit'>Submit</button>
-        </form>
-    </div>
+    return (
+        <div>
+            <form onSubmit = {handleSubmit}>
+                <input
+                    name= 'user_id'
+                    type= 'text'
+                    value= ''
+                    onChange= ''
+                />
+                <input
+                    name= 'description'
+                    type= 'text'
+                    value= ''
+                    onChange= ''
+                />
+                <input
+                    name= 'category'
+                    type= 'text'
+                    value= ''
+                    onChange= ''
+                />
+                <input
+                    name= 'reply'
+                    type= ''
+                    value= ''
+                    onChange= ''
+                />
+                <input
+                    name= 'solved'
+                    type= ''
+                    value= ''
+                    onChange= ''
+                />  
+                <input
+                    name= 'category'
+                    type= ''
+                    value= ''
+                    onChange= ''
+                />
+                <input
+                    name= 'solved_by'
+                    type= ''
+                    value= ''
+                    onChange= ''
+                />
+                <input
+                    name= 'username'
+                    type= ''
+                    value= ''
+                    onChange= ''
+                />
+                <button type= 'submit'>Submit</button>
+            </form>
+        </div>
     )
-  }
 }
+
 export default CreateTicket;
