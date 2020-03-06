a7901978759ffcd96395bac787dfd0dc15a811aa
BenHall-7 <benjaminjahall@gmail.com>
2020-03-05 23:57:43 -0500

    Simplify and render form

--- dev-desk/src/components/CreateTicket.js
+++ dev-desk/src/components/CreateTicket.js
@@ -1,76 +1,72 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import axiosWithAuth from 'axios';
+import UserContext from '../contexts/UserContext';
 
 const CreateTicket = () => {
+    const {user} = useContext(UserContext);
+
     const [creatingTicket, setCreatingTicket] = useState({
-        user_id: '',
+        user_id: user.id,
+        username: user.username,
+        
+        category: "",
         description: "",
         urgency: "",
+
         reply: "",
         solved: false,
-        category: "",
         solved_by: "",
-        username: ""
-    })
+    });
 
     const handleSubmit = e => {
-    axiosWithAuth()
-    .post('https://daniels-dev-desk-backend.herokuapp.com/api/tickets', creatingTicket)
-    .then(response => {
-        console.log('response', response);        
-    })};
+        e.preventDefault();
+        axiosWithAuth()
+            .post('https://daniels-dev-desk-backend.herokuapp.com/api/tickets', creatingTicket)
+            .then(response => {
+                console.log('response', response);        
+            })
+    };
+
+    const handleChange = e => {
+        setCreatingTicket({
+            ...creatingTicket,
+            [e.target.name]: e.target.value
+        });
+    };
     
     return (
         <div>
             <form onSubmit = {handleSubmit}>
-                <input
-                    name= 'user_id'
-                    type= 'text'
-                    value= ''
-                    onChange= ''
-                />
-                <input
-                    name= 'description'
-                    type= 'text'
-                    value= ''
-                    onChange= ''
-                />
-                <input
-                    name= 'category'
-                    type= 'text'
-                    value= ''
-                    onChange= ''
-                />
-                <input
-                    name= 'reply'
-                    type= ''
-                    value= ''
-                    onChange= ''
-                />
-                <input
-                    name= 'solved'
-                    type= ''
-                    value= ''
-                    onChange= ''
-                />  
-                <input
-                    name= 'category'
-                    type= ''
-                    value= ''
-                    onChange= ''
-                />
-                <input
-                    name= 'solved_by'
-                    type= ''
-                    value= ''
-                    onChange= ''
-                />
-                <input
-                    name= 'username'
-                    type= ''
-                    value= ''
-                    onChange= ''
-                />
+                <label>
+                    Category:
+                    <input
+                        name= 'category'
+                        type= 'text'
+                        value= {creatingTicket.category}
+                        onChange= {handleChange}
+                    />
+                </label>
+                <label>
+                    Description:
+                    <input
+                        name= 'description'
+                        type= 'text'
+                        value= {creatingTicket.description}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Urgency:
+                    <select
+                        name='urgency'
+                        value={creatingTicket.urgency}
+                        onChange={handleChange}
+                    >
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                    </select>
+                </label>
                 <button type= 'submit'>Submit</button>
             </form>
         </div>
